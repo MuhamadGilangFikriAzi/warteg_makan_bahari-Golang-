a8c751e816d61388c2e83d389594cd60fc9605fb
Muhamad Gilang Fikri Azi <mgfa9802@gmail.com>
2022-03-31 13:19:21 +0700

    rename file usecase

--- delivery/middleware/app_middleware.go
+++ delivery/middleware/app_middleware.go
@@ -1,7 +1,7 @@
 package middleware
 
 import (
-	"fmt"
+	"WMB/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"net/http"
@@ -9,13 +9,13 @@ import (
 
 func AuthUser(requiredToken string, sqlxdb *sqlx.DB) bool {
 	var countAuthUser int
-	fmt.Println(requiredToken)
 	err := sqlxdb.Get(&countAuthUser, "select count(*) from users where token = $1", requiredToken)
-	fmt.Println(countAuthUser)
 	if err != nil {
+		logger.Log.Err(err).Msg("Database not conected")
 		panic("database not connected")
 	}
 	if countAuthUser == 0 {
+		logger.Log.Err(err).Msg("Token Auth not found")
 		return true
 	}
 	return false
